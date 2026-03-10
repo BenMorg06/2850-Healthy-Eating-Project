f134884058aae93925f8cf8b45c3b3eb870d702f
BenMorg06 <b_morgan04@outlook.com>
2026-03-10 17:55:38 +0000

    filler user model added to db, flask app loads with db

--- flask-app/flaskr/__init__.py
+++ flask-app/flaskr/__init__.py
@@ -1,14 +1,16 @@
 import os
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
     if test_config is None:
@@ -20,6 +22,8 @@ def create_app(test_config=None):
 
     # ensure the instance folder exists
     os.makedirs(app.instance_path, exist_ok=True)
+
+    db.init_app(app)
 
     # a simple page that says hello
     @app.route('/hello')
